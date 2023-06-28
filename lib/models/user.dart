9f948ccde559fbf15e91282aeb679cdc5b1257f2
Youssef <youssef.hussein.business@gmail.com>
2023-06-28 13:40:02 +0300

    first commit

---
+++ lib/models/user.dart
@@ -0,0 +1,14 @@
+//Creating a class user to store the data;
+class User {
+  final int? id;
+  final int? userId;
+  final String? title;
+  final String? body;
+
+  User({
+    this.id,
+    this.userId,
+    this.title,
+    this.body,
+  });
+}
